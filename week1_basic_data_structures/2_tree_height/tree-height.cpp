b2979b3c45ef30e096bb0899b636dd37ad5a249f
pujith22 <pujith22.sde@gmail.com>
2025-05-13 23:42:09 +0530

    balaced parenthesis and tree height assignments completed

--- week1_basic_data_structures/2_tree_height/tree-height.cpp
+++ week1_basic_data_structures/2_tree_height/tree-height.cpp
@@ -23,12 +23,27 @@ public:
     }
 };
 
+int height(Node* root)
+{
+  if(root==NULL)
+    return 0;
+  int maxheight = 0;
+  for(Node* i: root->children)
+  {
+    int height_of_i_th_child = height(i);
+    if(maxheight < height_of_i_th_child)
+      maxheight = height_of_i_th_child;
+  }
+  return 1+maxheight;
+}
+
 
 int main_with_large_stack_space() {
   std::ios_base::sync_with_stdio(0);
   int n;
   std::cin >> n;
 
+  Node* rootnode = NULL;
   std::vector<Node> nodes;
   nodes.resize(n);
   for (int child_index = 0; child_index < n; child_index++) {
@@ -36,17 +51,21 @@ int main_with_large_stack_space() {
     std::cin >> parent_index;
     if (parent_index >= 0)
       nodes[child_index].setParent(&nodes[parent_index]);
+    else
+      rootnode = &nodes[child_index];
     nodes[child_index].key = child_index;
   }
 
   // Replace this code with a faster implementation
-  int maxHeight = 0;
-  for (int leaf_index = 0; leaf_index < n; leaf_index++) {
-    int height = 0;
-    for (Node *v = &nodes[leaf_index]; v != NULL; v = v->parent)
-      height++;
-    maxHeight = std::max(maxHeight, height);
-  }
+  // int maxHeight = 0;
+  // for (int leaf_index = 0; leaf_index < n; leaf_index++) {
+  //   int height = 0;
+  //   for (Node *v = &nodes[leaf_index]; v != NULL; v = v->parent)
+  //     height++;
+  //   maxHeight = std::max(maxHeight, height);
+  // }
+
+  int maxHeight = height(rootnode);
     
   std::cout << maxHeight << std::endl;
   return 0;
