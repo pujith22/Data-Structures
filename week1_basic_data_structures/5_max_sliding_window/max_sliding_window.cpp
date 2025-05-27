e163442cf69ea1a8a51d7254592754310f5111b6
pujith22 <pujith22.sde@gmail.com>
2025-05-27 21:43:37 +0530

    sliding window maximum problem completed

--- week1_basic_data_structures/5_max_sliding_window/max_sliding_window.cpp
+++ week1_basic_data_structures/5_max_sliding_window/max_sliding_window.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
+
 using std::cin;
 using std::cout;
 using std::vector;
 using std::max;
+
+void pujith22()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
 
 void max_sliding_window_naive(vector<int> const & A, int w) {
     for (size_t i = 0; i < A.size() - w + 1; ++i) {
@@ -18,8 +31,29 @@ void max_sliding_window_naive(vector<int> const & A, int w) {
     return;
 }
 
+void max_sliding_window(vector<int> const& arr, int w)
+{
+    deque<int> d;
+    for(int i=0;i<arr.size();i++)
+    {
+        // remove elements that are no more in the current window
+        while(d.size()>0 and d.front()<=i-w)
+            d.pop_front();
+
+        // here we are maintaining the monotonicity of deque, i.e all elements would be in decreasing order.
+        while(d.size()>0 and arr[d.back()]<=arr[i])
+            d.pop_back();
+
+        d.push_back(i);
+
+        // front will always contain the max element of the current window (recall that the elements are in decreasing order)
+        if(i>=w-1)
+            cout<<arr[d.front()]<<" ";
+    }
+}
 
 int main() {
+    pujith22();
     int n = 0;
     cin >> n;
 
@@ -30,7 +64,7 @@ int main() {
     int w = 0;
     cin >> w;
 
-    max_sliding_window_naive(A, w);
+    max_sliding_window(A, w);
 
     return 0;
 }
