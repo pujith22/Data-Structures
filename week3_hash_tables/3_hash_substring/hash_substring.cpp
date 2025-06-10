ef4aa07912c5b8e41dbd893e9bce846259cd6cda
pujith22 <pujith22.sde@gmail.com>
2025-06-10 22:31:16 +0530

    rabin karb string matching algorithm using rolling hash done

--- week3_hash_tables/3_hash_substring/hash_substring.cpp
+++ week3_hash_tables/3_hash_substring/hash_substring.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#define int unsigned long long
 
 using std::string;
-typedef unsigned long long ull;
+// typedef unsigned long long ull;
+
+using std::cin;
+using std::cout;
+
+// void pujith22()
+// {
+//     #ifndef ONLINE_JUDGE
+//         freopen("input.txt","r",stdin);
+//         freopen("output.txt","w",stdout);
+//     #endif
+//     cin.tie(NULL);
+//     cout.tie(NULL);
+//     std::ios_base::sync_with_stdio(false);
+// }
 
 struct Data {
     string pattern, text;
@@ -24,14 +39,38 @@ void print_occurrences(const std::vector<int>& output) {
 std::vector<int> get_occurrences(const Data& input) {
     const string& s = input.pattern, t = input.text;
     std::vector<int> ans;
-    for (size_t i = 0; i + s.size() <= t.size(); ++i)
-        if (t.substr(i, s.size()) == s)
-            ans.push_back(i);
+    if(s.size()>t.size())
+        return ans;
+    int prime = 31LL;
+    int mod = 1e9+7LL;
+    int patternHash = 0;
+    int textRollingHash = 0;
+    for(int i=0;i<s.size();i++)
+        patternHash = (patternHash*prime + s[i]+mod)%mod;
+
+    int primePowN = 1;
+    for(int i=0;i<s.size();i++)
+    {
+        textRollingHash = (textRollingHash*prime+t[i]+mod)%mod;
+        primePowN *= prime;
+        primePowN %= mod;
+    }
+    
+    for (size_t i = s.size(); i <= t.size(); ++i)
+    {
+        if (patternHash==textRollingHash && t.substr(i-s.size(),s.size())==s)
+            ans.push_back(i-s.size());
+        if(i==t.size())
+            break;
+        textRollingHash = ((textRollingHash*prime)%mod + t[i]+mod)%mod;
+        textRollingHash = (textRollingHash - (t[i-s.size()]*primePowN)%mod + mod)%mod;
+    }
     return ans;
 }
 
 
-int main() {
+int32_t main() {
+    // pujith22();
     std::ios_base::sync_with_stdio(false);
     print_occurrences(get_occurrences(read_input()));
     return 0;
