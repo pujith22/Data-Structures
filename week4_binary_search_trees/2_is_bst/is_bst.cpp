bf768ca37c2a89e1eaeca1ca8923aa0bbbef221f
pujith22 <pujith22.sde@gmail.com>
2025-06-30 21:47:28 +0530

    balanced search tree checking algorithm implemented

--- week4_binary_search_trees/2_is_bst/is_bst.cpp
+++ week4_binary_search_trees/2_is_bst/is_bst.cpp
@@ -1,12 +1,28 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#define int long long
 
+// using namespace std;
 using std::cin;
 using std::cout;
 using std::endl;
 using std::vector;
+using std::pair;
+using std::make_pair;
+using std::min;
+using std::max;
 
+// void pujith22()
+// {
+//   #ifndef ONLINE_JUDGE
+//     freopen("input.txt","r",stdin);
+//     freopen("output.txt","w",stdout);
+//   #endif
+//   cin.tie(NULL);
+//   cout.tie(NULL);
+//   ios_base::sync_with_stdio(false);
+// }
 struct Node {
   int key;
   int left;
@@ -16,12 +32,43 @@ struct Node {
   Node(int key_, int left_, int right_) : key(key_), left(left_), right(right_) {}
 };
 
-bool IsBinarySearchTree(const vector<Node>& tree) {
-  // Implement correct algorithm here
-  return true;
+pair<bool,pair<int,int>> helper(const vector<Node>& tree, const Node& node)
+{
+  if(node.left==-1 && node.right==-1)
+    return {true,{node.key,node.key}};
+  int MIN = -(1LL<<31);
+  int MAX = (1LL<<31)-1LL;
+  auto left = make_pair(true,make_pair(MAX,MIN));
+  auto right = left;
+  if(node.left!=-1)
+    left = helper(tree,tree[node.left]);
+  if(node.right!=-1)
+    right = helper(tree,tree[node.right]);
+  bool ans = true;
+  ans = ans && left.first && right.first;
+  int left_min = left.second.first;
+  int left_max = left.second.second;
+  int right_min = right.second.first;
+  int right_max = right.second.second;
+  // cout<<left_min<<", "<<left_max<<", "<<right_min<<", "<<right_max<<"key: "<<node.key<<"\n";
+  ans = ans && (left_max<node.key && right_min>node.key);
+  int tree_min = min(left_min,right_min);
+  tree_min = min(tree_min,node.key);
+  int tree_max = max(right_max,left_max);
+  tree_max = max(tree_max,node.key);
+  return {ans,{tree_min,tree_max}};
 }
 
-int main() {
+bool IsBinarySearchTree(const vector<Node>& tree) {
+  // Implement correct algorithm here
+  if(tree.size()<=1)
+    return true;
+  auto ans = helper(tree,tree[0]);
+  return ans.first;
+}
+
+int32_t main() {
+  // pujith22();
   int nodes;
   cin >> nodes;
   vector<Node> tree;
@@ -30,7 +77,7 @@ int main() {
     cin >> key >> left >> right;
     tree.push_back(Node(key, left, right));
   }
-  if (IsBinarySearchTree(tree) {
+  if (IsBinarySearchTree(tree)) {
     cout << "CORRECT" << endl;
   } else {
     cout << "INCORRECT" << endl;
